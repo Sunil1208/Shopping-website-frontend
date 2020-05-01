d2dd0efb82f608550e323a8c33c5ae16b77d9e70
Sunil1208 <skmy03@gmail.com>
2020-05-02 00:20:39 +0530

    Signup

--- src/core/Base.js
+++ src/core/Base.js
@@ -3,7 +3,7 @@ import Menu from './Menu';
 const Base = ({
     title = "My Title",
     description="My description",
-    className="bg-dark text-white p-4",
+    className="bg-dark text-white p-0",
     children
 }) => {
     return(
@@ -14,8 +14,8 @@ const Base = ({
                     <h2 className="display-4">{title}</h2>
                     <p className="lead">{description}</p>
                 </div>
-                <div className={className}>{children}</div>>
-                </div>
+                <div className={className}>{children}</div>
+            </div>
             <footer className="footer bg-dark mt-auto py-3">
                 <div className="container-fluid bg-success text-white text-center py-3">
                     <h4>If you got any questions, feel free to reach out!</h4>
