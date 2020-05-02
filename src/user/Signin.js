48dc863670f0d2b62842cc48ff51c7d90636cb1f
Sunil1208 <skmy03@gmail.com>
2020-05-02 16:14:47 +0530

    Authentication, Private/Admin Routing and Page Restrictions

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,8 +1,77 @@
 import React, {useState} from 'react';
 import Base from '../core/Base';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { signin, authenticate, isAuthenticated } from '../auth/helper'
 
 const Signin = () => {
+
+    const [values, setValues ] = useState({
+        email:"skmy03@mail.com",
+        password:"123456",
+        error:"",
+        loading: false,
+        didRedirect: false
+    })
+
+    const { email, password, error, loading, didRedirect } = values;
+    const { user } = isAuthenticated();
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const performRedirect = () => {
+        if(didRedirect){
+            if(user && user.role ===1){
+                return <Redirect to="/admin/dashboard" />
+            } else {
+                return <Redirect to="/user/dashboard" />
+            }
+        }
+        if(isAuthenticated()) {
+            return <Redirect to="/" />
+        }
+    }
+
+    const onSubmit = event => {
+          event.preventDefault();
+          setValues({...values, error: false, loading:true})
+          signin({email, password})
+            .then(data => {
+                if(data.error){
+                    setValues({...values, error: data.error, loading:false})
+                } else {
+                    authenticate(data, () => {
+                        setValues({
+                            ...values,
+                            didRedirect:true
+                        })
+                    })
+                }
+            })
+            .catch(console.log("Signin request failed"))
+    }
+
+    const loadingMessage = () => {
+        return (
+            loading && (
+                <div className="alert alert-info">
+                    <h2>Loading</h2>
+                </div>
+            )
+        )
+    }
+  
+    const errorMessage = () => {
+        return(
+          <div className="row">
+              <div className="col-md-6 offset-sm-3 text-left">
+                  <div className="alert alert-danger" style={{display: error ? "" : "none"}}>
+                      {error}
+                  </div>
+              </div>
+          </div>)
+  }
 
     const signInForm = () => {
         return(
@@ -11,13 +80,13 @@ const Signin = () => {
                     <form>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input className="form-control" type="email" placeholder="Enter your email address" />
+                            <input onChange={handleChange("email")} value={email} className="form-control" type="email" placeholder="Enter your email address" />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input className="form-control" type="password" placeholder="Enter a password" />
+                            <input onChange={handleChange("password")} value={password} className="form-control" type="password" placeholder="Enter a password" />
                         </div>
-                        <button className="btn btn-success btn-block">Login</button>
+                        <button onClick={onSubmit} className="btn btn-success btn-block">Login</button>
                     </form>
                 </div>
             </div>
@@ -26,7 +95,11 @@ const Signin = () => {
 
     return(
         <Base title="Sign in page" description="A page for user to sign in!">
+        {loadingMessage()}
+        {errorMessage()}
         {signInForm()}
+        {performRedirect()}
+        <p className="text-white text-center">{JSON.stringify(values)}</p>
         </Base>
     )
 }
